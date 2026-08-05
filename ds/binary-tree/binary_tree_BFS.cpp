c64355363914e65a99e7c0b734574ce76016a4c4
Noman <molla.noman23@gmail.com>
2026-08-06 02:54:09 +0600

    add binary tree BFS and DFS

---
+++ ds/binary-tree/binary_tree_BFS.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *binary_tree_input()
+{
+    int val;
+    cin >> val;
+    Node *root = new Node(val);
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        auto p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *l_node, *r_node;
+
+        if (l != -1)
+        {
+            l_node = new Node(l);
+        }
+        else
+        {
+            l_node = NULL;
+        }
+
+        if (r != -1)
+        {
+            r_node = new Node(r);
+        }
+        else
+        {
+            r_node = NULL;
+        }
+
+        p->left = l_node;
+        p->right = r_node;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+
+    return root;
+}
+
+void level_order_print(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        auto f = q.front();
+        cout << f->val;
+        q.pop();
+
+        if (f->left)
+        {
+            q.push(f->left);
+        }
+        if (f->right)
+        {
+            q.push(f->right);
+        }
+    }
+}
+
+int main()
+{
+    auto root = binary_tree_input();
+
+    level_order_print(root);
+
+    return 0;
+}
