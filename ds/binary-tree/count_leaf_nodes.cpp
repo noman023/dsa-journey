57ae0fe5e79aa81a69b7543e3b5b07b432fc1db9
Noman <molla.noman23@gmail.com>
2026-08-06 03:22:02 +0600

    add nodes and leaf nodes count

---
+++ ds/binary-tree/count_leaf_nodes.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *binary_tree_input()
+{
+    int val;
+    cin >> val;
+    Node *root = new Node(val);
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        auto p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *l_node, *r_node;
+
+        if (l != -1)
+        {
+            l_node = new Node(l);
+        }
+        else
+        {
+            l_node = NULL;
+        }
+
+        if (r != -1)
+        {
+            r_node = new Node(r);
+        }
+        else
+        {
+            r_node = NULL;
+        }
+
+        p->left = l_node;
+        p->right = r_node;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+
+    return root;
+}
+
+int count_leaf_nodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+
+    int l = count_leaf_nodes(root->left);
+    int r = count_leaf_nodes(root->right);
+
+    return l + r;
+}
+
+int main()
+{
+    auto root = binary_tree_input();
+    int leaf_nodes = count_leaf_nodes(root);
+
+    cout << leaf_nodes << endl;
+    return 0;
+}
