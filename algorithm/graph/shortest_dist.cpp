7c41f2c9ed363353db8ee499fba6882eb5e46690
Noman <molla.noman23@gmail.com>
2026-08-23 20:15:07 +0600

    single source shortest distance

---
+++ algorithm/graph/shortest_dist.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[5];
+bool visited[5];
+int level[5];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    level[src] = 0;
+
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+
+        for (int child : adj_list[parent])
+        {
+            if (!visited[child])
+            {
+                q.push(child);
+                visited[child] = true;
+                level[child] = level[parent] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int v, e;
+    cin >> v >> e;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(visited, false, sizeof(visited));
+    memset(level, -1, sizeof(level));
+
+    int src, dist;
+    cin >> src >> dist;
+
+    bfs(src);
+
+    cout << level[dist];
+
+    return 0;
+}
